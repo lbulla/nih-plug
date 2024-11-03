45f4e5c2063ffea868c6b58aa63af23be7abf508
lbulla <118393328+lbulla@users.noreply.github.com>
2024-11-03 11:48:12 +0100

    Fix crash in spectral_compressor during AudioPluginHost's scan (JUCE)

--- plugins/spectral_compressor/src/analyzer.rs
+++ plugins/spectral_compressor/src/analyzer.rs
@@ -42,11 +42,11 @@ pub struct AnalyzerData {
     ///
     /// This data is taken directly from the envelope followers, so it has the same rise and fall
     /// time as what is used by the compressors.
-    pub envelope_followers: [f32; crate::MAX_WINDOW_SIZE / 2 + 1],
+    pub envelope_followers: Box<[f32]>,
     /// The gain different applied to each band, in decibels. Alternatively, the negative gain
     /// reduction. Positive values mean that a band becomes louder, and negative values mean a band
     /// got attenuated. Does not (and should not) factor in the output gain.
-    pub gain_difference_db: [f32; crate::MAX_WINDOW_SIZE / 2 + 1],
+    pub gain_difference_db: Box<[f32]>,
     // TODO: Include the threshold curve. Decide on whether to only visualizer the 'global'
     //       threshold curve or to also show the individual upwards/downwards thresholds. Or omit
     //       this and implement it in a nicer way for the premium Spectral Compressor.
@@ -58,8 +58,8 @@ impl Default for AnalyzerData {
             curve_params: CurveParams::default(),
             curve_offsets_db: (0.0, 0.0),
             num_bins: 0,
-            envelope_followers: [0.0; crate::MAX_WINDOW_SIZE / 2 + 1],
-            gain_difference_db: [0.0; crate::MAX_WINDOW_SIZE / 2 + 1],
+            envelope_followers: [0.0; crate::MAX_WINDOW_SIZE / 2 + 1].into(),
+            gain_difference_db: [0.0; crate::MAX_WINDOW_SIZE / 2 + 1].into(),
         }
     }
 }
